60284bdf0a9a978c604804950ba4789c5b37b363
Anton Hosgood <104061097+antonhosgood@users.noreply.github.com>
2022-11-12 18:15:49 +0100

    Finalise CoronaWiki exploration

--- data_processing.py
+++ data_processing.py
@@ -116,28 +116,50 @@ def process_transport_data(df, countries):
     return df
 
 
-def get_pageviews(df, lang, topic):
+def get_pageviews(df, lang, topic, measure="sum"):
     """Combines the views from desktop and mobile Wikipedia pages for a given language and topic.
 
     Args:
         df: pageviews (dataframe).
         lang: language code (string).
-        topic: page topic (string).
+        topic: page topic or "covid" (string).
+        measure: "sum" or "percent"
 
     Returns:
         Series of dates and topic pageviews.
     """
 
     # Select desktop and mobile pageviews
-    df1 = df[lang]["topics"][topic]["sum"]
-    df2 = df[lang + ".m"]["topics"][topic]["sum"]
+    if topic == "covid":
+        df1 = df[lang]["covid"][measure]
+        df2 = df[lang + ".m"]["covid"][measure]
+    else:
+        df1 = df[lang]["topics"][topic][measure]
+        df2 = df[lang + ".m"]["topics"][topic][measure]
 
     # defaultdict in case pageview count for a specific day only appears in one series
     total_views = defaultdict(int, df1)
 
     # Combine pageviews
-    for date, views in df2.items():
-        total_views[date] += views
+    if measure == "sum":
+        for date, views in df2.items():
+            total_views[date] += views
+
+    elif measure == "percent":
+        if topic == "covid":
+            df1_counts = defaultdict(float, df[lang]["covid"]["sum"])
+            df2_counts = defaultdict(float, df[lang + ".m"]["covid"]["sum"])
+        else:
+            df1_counts = defaultdict(float, df[lang]["topics"][topic]["sum"])
+            df2_counts = defaultdict(float, df[lang + ".m"]["topics"][topic]["sum"])
+
+        for date, views in df2.items():
+            total_views[date] = (
+                (total_views[date] * df1_counts[date] + views * df2_counts[date])
+                / (df1_counts[date] + df2_counts[date])
+                if df1_counts[date] + df2_counts[date] != 0
+                else 0
+            )
 
     # Convert to dataframe
     total_views = pd.DataFrame(total_views.items(), columns=["date", "pageviews"])
