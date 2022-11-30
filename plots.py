59c61a7a467b0cde3eac29733fd5685e287cf79d
Anton Hosgood <104061097+antonhosgood@users.noreply.github.com>
2022-11-30 23:17:03 +0100

    Create plots.py

---
+++ plots.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
+
+
+def plot_silhouettes(X, k_min, k_max):
+    """Plot Silhouette Scores for a range of clusters between `k_min` and `k_max` inclusive."""
+
+    silhouettes = []
+
+    for k in range(k_min, k_max + 1):
+        # Cluster the data and assign labels
+        labels = KMeans(n_clusters=k, random_state=0).fit_predict(X)
+        # Compute the mean Silhouette Coefficient over all samples
+        score = silhouette_score(X, labels)
+        silhouettes.append({"k": k, "score": score})
+
+    # Convert to dataframe
+    silhouettes = pd.DataFrame(silhouettes)
+
+    # Plot the data
+    plt.plot(silhouettes.k, silhouettes.score)
+    plt.title("Silhouette Score for each value of K")
+    plt.xlabel("K")
+    plt.ylabel("Silhouette score")
+
+
+def plot_mobility_response(df, labels=None):
+    """Plot response time and duration of reduced mobility period of countries. Optionally group the countries by `labels`."""
+
+    ax = sns.scatterplot(x=df["Response time"], y=df["Reduced mobility"], hue=labels)
+
+    for idx, row in df.iterrows():
+        ax.text(row["Response time"] + 0.5, row["Reduced mobility"], idx)
+
+    plt.title("Reduced mobility vs. Response time")
+    plt.xlabel("Response time (days)")
+    plt.ylabel("Reduced mobility (days)")
